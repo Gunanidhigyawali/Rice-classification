79f41bd89be21dfb39900f07a689e72e5bda0ce5
Gudu Gyawali <saroz>
2022-06-14 17:06:51 +0545

    main.py created

---
+++ main.py
@@ -0,0 +1,32 @@
+
+import os
+from utils.config_loader import load_config,get_base_dic
+import csv
+
+config_loc=os.path.join(get_base_dic(),"configs","network.json")
+config=load_config(config_loc)
+if not config["hardware"]["GPU"]["active"]:
+    os.environ["CUDA_VISIBLE_DEVICES"]="-1" 
+
+from model.custom_model import BirdNet
+
+
+obj=BirdNet(config)
+
+# training from starting
+obj.train()
+
+# resume the model with saved model
+# obj.load_model()
+# obj.train()
+obj.save_model()
+
+#testing
+obj.evaluate()
+
+
+#predict
+# obj.predict("dataset/train/ABBOTTS BABBLER/001.jpg")
+
+# load_weight
+# obj.load_weight()
